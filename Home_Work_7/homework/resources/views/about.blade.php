a614497291f46d2f188170e53a9ebac0444886de
Administrator <renzo@Dan-PC>
2024-01-31 01:18:08 +0300

    update

---
+++ Home_Work_7/homework/resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!-- resources/views/about.blade.php -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Me</title>
+</head>
+<body>
+<h1>About me</h1>
+<p>1. Name - Dan</p>
+<p>2. Family - Кошевой</p>
+<p>3. Profession - Program Developer</p>
+</body>
+</html>
