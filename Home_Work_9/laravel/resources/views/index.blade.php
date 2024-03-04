2fd2b4c8d13bd0b2ad6f0c96fc5a2958d9fbf2dd
Dan Koshevoy <dankoshevoy2001@gmail.com>
2024-03-04 04:05:01 +0300

    up

--- Home_Work_9/laravel/resources/views/index.blade.php
+++ Home_Work_9/laravel/resources/views/index.blade.php
@@ -47,6 +47,7 @@
             @endif
         </div>
     @endif
+
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-custom">
