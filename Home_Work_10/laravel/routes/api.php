a6406260a8dca38c7dee2273d4509d38e9bce6f6
Dan Koshevoy <dankoshevoy2001@gmail.com>
2024-03-27 05:44:29 +0300

    up

---
+++ Home_Work_10/laravel/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+require_once 'Authentication/authentication_api.php';
---
+++ Home_Work_10/laravel/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+require_once 'Authentication/authentication_view.php';
