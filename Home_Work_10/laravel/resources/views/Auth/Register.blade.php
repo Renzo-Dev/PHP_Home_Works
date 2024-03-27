4491e4cdfcab0fa6dde18b9199acbe50d8c0f7f4
Dan Koshevoy <dankoshevoy2001@gmail.com>
2024-03-27 06:59:05 +0300

    up

--- Home_Work_10/laravel/resources/views/Auth/Register.blade.php
+++ Home_Work_10/laravel/resources/views/Auth/Register.blade.php
@@ -5,13 +5,19 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="../../css/test.css">
+    <link href="{{ asset('css/registration.css') }}" rel="stylesheet" type="text/css">
     <title>Registration</title>
 </head>
 <body>
 <h1>Регистрация</h1>
-<form>
-
+<form id="formInputData">
+    @csrf
+    <input type="text" name="name" placeholder="Name">
+    <input type="email" name="email" placeholder="Email">
+    <input type="password" name="password" autocomplete="off" placeholder="Password">
+    <button type="submit">Регистрация</button>
 </form>
+<a href="{{route('login')}}">Вход</a>
+<script src="{{ asset('js/registration.js') }}"></script>
 </body>
 </html>
