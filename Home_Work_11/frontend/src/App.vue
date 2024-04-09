62dc7d193fd13a8047babafebec6e1ddfb66d1ee
Dan Koshevoy <dankoshevoy2001@gmail.com>
2024-04-10 01:28:23 +0300

    up

---
+++ Home_Work_11/frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>Hello World</h1>
+</template>
+
+<script lang="js">
+
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+  components: {},
+  setup() {
+  }
+})
+
+</script>
+
+<style lang="css">
+
+</style>
